6126c57ac98e4e0458cb206a67febc2ca477c944
Bighsueh <40841121@gm.nfu.edu.tw>
2021-08-04 23:01:37 +0800

    add __construct()

--- app/Http/Controllers/LineController.php
+++ app/Http/Controllers/LineController.php
@@ -3,16 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use LINE\LINEBot;
+use LINE\LINEBot\Event\MessageEvent;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 
 class LineController extends Controller
 {
-    //
-    public function index(Request $request)
+    private $client;
+    private $bot;
+    private $channel_access_token;
+    private $channel_secret;
+
+    public function __construct()
     {
-        $params = $request->all();
-        logger(json_encode($params, JSON_UNESCAPED_UNICODE));
-//        error_log(json_encode('1'));
-        return response('ok', '200');
+        //用env內的Channel_access_token & Channel secret建立一個LineBot物件
+        $this->channel_access_token = env('CHANNEL_ACCESS_TOKEN');
+        $this->channel_secret = env('CHANNEL_SECRET');
+        $httpClient = new CurlHTTPClient($this->channel_access_token);
+        $this->bot = new LINEBot($httpClient, ['channelSecret' => $this->channel_secret]);
+        $this->client = $httpClient;
     }
 
+    public function index(Request $request)
+    {
+      //
+    }
 }
